e3350a42149096b1aeab31858d98d68030eb7af7
Nitya Dwivedi <96773479+ditya17@users.noreply.github.com>
2022-06-21 20:57:07 +0530

    Create inverted_number_pattern.cpp

---
+++ patterns_2/inverted_number_pattern.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+    int n;
+    cin>>n;
+    for (int i =0;i<n;i++){
+        for (int j =n;j>i;j--){
+            cout<<n-i;
+        }
+        cout<<endl;
+    }
+}
