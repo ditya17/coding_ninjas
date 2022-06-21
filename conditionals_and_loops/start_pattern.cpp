bc11bf1a6e465bbf379c82da36320d03f57968c9
Nitya Dwivedi <96773479+ditya17@users.noreply.github.com>
2022-06-21 20:46:12 +0530

    Create start_pattern.cpp

---
+++ conditionals_and_loops/start_pattern.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin>>n;
+    for (int i =1;i<=n;i++){
+        cout<< string( n-i, ' ');
+        cout<< string (i+i-1, '*');
+    cout<<endl;
+    }
+}
+
