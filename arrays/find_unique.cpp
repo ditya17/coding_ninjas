cba856e78473f71802555b03e64574078253267e
Nitya Dwivedi <96773479+ditya17@users.noreply.github.com>
2022-07-02 22:48:09 +0530

    Create find_unique.cpp

---
+++ arrays/find_unique.cpp
@@ -0,0 +1,11 @@
+int findUnique(int *arr, int size)
+{
+    for (int i =0;i<size;i++){
+        int count = 0;
+        int key = arr[i];
+        for (int j =0;j<size;j++){
+            if (arr[j] == key) count++;
+        }
+    if (count == 1) return arr[i];
+    }
+}
