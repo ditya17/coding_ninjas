2504f0ae353990508b96daeaa5cd5ed5daf462ab
Nitya Dwivedi <96773479+ditya17@users.noreply.github.com>
2022-07-03 08:03:52 +0530

    Update insertionsort.cpp

--- searching_and_sorting/insertionsort.cpp
+++ searching_and_sorting/insertionsort.cpp
@@ -2,7 +2,7 @@ void insertionSort(int *input, int size)
 {
     for (int i=0;i<size;i++){
         int j = i;
-        while(j>=0){
+        while(j>0){
             if (input[j]<input[j-1]) swap(input[j],input[j-1]);
             j--;
         }
