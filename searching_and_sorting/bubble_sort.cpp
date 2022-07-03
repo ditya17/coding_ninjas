1a086cd70b0bae24c6e6a930fefe51cebc2f3a56
Nitya Dwivedi <96773479+ditya17@users.noreply.github.com>
2022-07-03 06:25:11 +0530

    Create bubble_sort.cpp

---
+++ searching_and_sorting/bubble_sort.cpp
@@ -0,0 +1,8 @@
+void bubbleSort(int *input, int size)
+{
+    for (int j =0;j<size;j++){
+        for (int i =0;i<size-1 - j;i++){
+            if (input[i] > input[i+1]) swap(input[i],input[i+1]);
+        }
+	}
+}
