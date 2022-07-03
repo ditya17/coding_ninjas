6981c935f6e219dfb2e4b2bc4c83827080ec513d
Nitya Dwivedi <96773479+ditya17@users.noreply.github.com>
2022-07-03 07:46:34 +0530

    Create merge_2_sorted_array.cpp

---
+++ searching_and_sorting/merge_2_sorted_array.cpp
@@ -0,0 +1,26 @@
+void merge(int *arr1, int size1, int *arr2, int size2, int *ans)
+{
+    int i =0,j=0,k=0;
+    while (i<=size1 -1 && j<=size2 -1){
+        if (arr1[i]<arr2[j]){
+            ans[k] = arr1[i];
+            k++;
+            i++;
+        }
+        else {
+            ans[k] = arr2[j];
+            k++;
+            j++;
+        }
+    }
+    while (j<size2){
+        ans[k] = arr2[j];
+        k++;
+        j++;
+    }
+     while ( i<size1){
+        ans[k] = arr1[i];
+        k++;
+        i++;
+    }
+}
