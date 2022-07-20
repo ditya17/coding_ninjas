bf4e69f59210d3d3ec1b8b86d69049ec97efe1ee
Nitya Dwivedi <96773479+ditya17@users.noreply.github.com>
2022-07-21 00:58:31 +0530

    Update sum_of_two_arrays.cpp

--- searching_and_sorting/sum_of_two_arrays.cpp
+++ searching_and_sorting/sum_of_two_arrays.cpp
@@ -1,6 +1,5 @@
 void sumOfTwoArrays(int *input1, int size1, int *input2, int size2, int *output)
 {
-    //Write your code here
     int a,sum,x,n =0;
     if (size1>size2) a = size1+1;
     else a=size2+1;
@@ -8,11 +7,31 @@ void sumOfTwoArrays(int *input1, int size1, int *input2, int size2, int *output)
         sum = input1[size1] + input2[size2] + n;
         x = sum%10;
         n = sum/10;
-        output[a-1] = x;
+        output[a] = x;
         
         size1--;
         size2--;
         a--;
     }
-    while (size1 ==0 && size!= )
+    while ( size2>= 0 ){
+        sum = input2[size2] + n;
+        x = sum%10;
+        n = sum/10;
+        output[a ] = x;
+        
+        size2--;
+        a--;
+    }
+    while ( size1>= 0 ){
+        sum = input1[size1] + n;
+        x = sum%10;
+        n = sum/10;
+        output[a ] = x;
+        
+        size1--;
+        a--;
+    }
+    if (n==1){
+        output[a] = n;
+    }
 }
